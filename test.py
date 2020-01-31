c5ddb17cde53fa961ca991e53dcfd17c79055c40
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2020-01-31 22:08:52 +0200

    Relax sympy test to allow for float inaccuracy

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import unittest
 
 from black_scholes_dp import *
 from black_scholes_ndp import *
+from sympy import erf, sqrt
 
 
 class MyTest(unittest.TestCase):
@@ -17,19 +18,20 @@ class MyTest(unittest.TestCase):
                          )
 
     def test_non_divident_paying_sympy(self):
-        self.assertEqual(euro_call_sym(50, 100, 1, 0.05, 0.25),
-                         -25.0*erf(1.22379436111989*sqrt(2)) - 22.5614712250357 +
-                         47.5614712250357*erf(1.34879436111989*sqrt(2))
-                         )
+        self.assertAlmostEqual(euro_call_sym(50, 100, 1, 0.05, 0.25),
+                               -25.0*erf(1.22379436111989*sqrt(2)) - 22.5614712250357 +
+                               47.5614712250357*erf(1.34879436111989*sqrt(2))
+                               )
 
-        self.assertEqual(euro_put_sym(50, 100, 1, 0.05, 0.25),
-                         45.150294959440842
-                         )
+        self.assertAlmostEqual(euro_put_sym(50, 100, 1, 0.05, 0.25),
+                               -25.0*erf(1.22379436111989*sqrt(2)) + 22.5614712250357 +
+                               47.5614712250357*erf(1.34879436111989*sqrt(2))
+                               )
 
-        self.assertEqual(sym_euro_vanilla(50, 100, 1, 0.05, 0.25, option='put'),
-                         -25.0*erf(1.22379436111989*sqrt(2)) + 22.5614712250357 +
-                         47.5614712250357*erf(1.34879436111989*sqrt(2))
-                         )
+        self.assertAlmostEqual(sym_euro_vanilla(50, 100, 1, 0.05, 0.25, option='put'),
+                               -25.0*erf(1.22379436111989*sqrt(2)) + 22.5614712250357 +
+                               47.5614712250357*erf(1.34879436111989*sqrt(2))
+                               )
         pass
 
     def test_divident_paying(self):
