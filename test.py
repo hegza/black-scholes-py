6a1609d41a9ffce1c1b15d5b0c212b9f3a1a56ca
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2020-01-31 21:50:18 +0200

    Write tests that reveal API breakage

--- test.py
+++ test.py
@@ -17,7 +17,19 @@ class MyTest(unittest.TestCase):
                          )
 
     def test_non_divident_paying_sympy(self):
-        # TODO: exact sympy variants untested
+        self.assertEqual(euro_call_sym(50, 100, 1, 0.05, 0.25),
+                         -25.0*erf(1.22379436111989*sqrt(2)) - 22.5614712250357 +
+                         47.5614712250357*erf(1.34879436111989*sqrt(2))
+                         )
+
+        self.assertEqual(euro_put_sym(50, 100, 1, 0.05, 0.25),
+                         45.150294959440842
+                         )
+
+        self.assertEqual(sym_euro_vanilla(50, 100, 1, 0.05, 0.25, option='put'),
+                         -25.0*erf(1.22379436111989*sqrt(2)) + 22.5614712250357 +
+                         47.5614712250357*erf(1.34879436111989*sqrt(2))
+                         )
         pass
 
     def test_divident_paying(self):
