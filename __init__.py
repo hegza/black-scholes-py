d55fa4f4a3252cc6856dac18bb3317dcc8efd2d7
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2020-02-03 19:32:03 +0200

    Implement a main-function that visits all functions

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from timeit import timeit
+
+from black_scholes_dp import *
+from black_scholes_ndp import *
+
+
+def main():
+    euro_vanilla_call(50, 100, 1, 0.05, 0.25)
+    euro_vanilla_put(50, 100, 1, 0.05, 0.25)
+    euro_vanilla(50, 100, 1, 0.05, 0.25, option='put')
+    black_scholes_call_div(50, 100, 1, 0.05, 0.06, 0.25)
+    black_scholes_put_div(50, 100, 1, 0.05, 0.06, 0.25)
+    euro_vanilla_dividend(50, 100, 1, 0.05, 0.06, 0.25, option='put')
