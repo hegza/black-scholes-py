4a415090ea9a0177e129ea5b38efb0a0dcf52ec1
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2020-02-08 19:26:51 +0200

    Divide timeit by number of iterations

--- bench.py
+++ bench.py
@@ -28,12 +28,12 @@ def bench_euro_vanilla_dividend():
     euro_vanilla_dividend(50, 100, 1, 0.05, 0.06, 0.25, option='put')
 
 
-print("euro_vanilla_call", str(timeit(bench_euro_vanilla_call, number=1000)))
-print("euro_vanilla_put", str(timeit(bench_euro_vanilla_put, number=1000)))
-print("euro_vanilla: " + str(timeit(bench_euro_vanilla, number=1000)))
+print("euro_vanilla_call", str(timeit(bench_euro_vanilla_call, number=1000)/1000.))
+print("euro_vanilla_put", str(timeit(bench_euro_vanilla_put, number=1000)/1000.))
+print("euro_vanilla: " + str(timeit(bench_euro_vanilla, number=1000)/1000.))
 print("black_scholes_call_div", str(
-    timeit(bench_black_scholes_call_div, number=1000)))
+    timeit(bench_black_scholes_call_div, number=1000)/1000.))
 print("black_scholes_put_div", str(
-    timeit(bench_black_scholes_put_div, number=1000)))
+    timeit(bench_black_scholes_put_div, number=1000)/1000.))
 print("euro_vanilla_dividend: " +
-      str(timeit(bench_euro_vanilla_dividend, number=1000)))
+      str(timeit(bench_euro_vanilla_dividend, number=1000)/1000.))
